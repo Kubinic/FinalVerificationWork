7c7951498a9e430a6db4ec451f52ef471bad49cd
Kubinic <Osiptsov84@gmail.com>
2023-04-01 18:17:30 +0700

    Метод Выбирающий из массива строки длинной не болие 3х символов и формирующий из них новый массив

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string [] ArrString = {"*", "One", "0", "Two", "three", "99", "nine", "ten","eleven"};
+string []ansver = SortLengArr (ArrString);
+Console.WriteLine(string.Join(" ", ArrString));
+Console.WriteLine();
+Console.WriteLine(string.Join(" ", ansver));
+string [] SortLengArr(string[] arr)
+{
+    int index = 0;
+
+    string[] StrArr = new string[arr.Length];
+    for (int i = 0; i < ArrString.Length; i++)
+    {
+
+        int num = ArrString[i].Length;
+        if (num <= 3)
+        {
+            StrArr[index] = arr[i];
+            index++;
+        }
+    }
+    return StrArr;
+}
